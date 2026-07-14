1afd0bf5c6f5dd19847da72b067b359bacf0d5fd
iomatix <mateuszwypchlak@gmail.com>
2026-07-15 00:19:12 +0200

    11.2.0

--- DoorRestartSystem/Plugin.cs
+++ DoorRestartSystem/Plugin.cs
@@ -33,7 +33,7 @@ namespace DoorRestartSystem
         public override string Author => "iomatix";
         public override string Name => "DoorRestartSystem";
         public override string Description => "Automated door lockdown and facility containment system.";
-        public override Version Version => new Version(11, 1, 2);
+        public override Version Version => new Version(11, 2, 0);
         public override Version RequiredApiVersion => new Version(1, 1, 7);
 
         public bool Debug => Config.Debug;
